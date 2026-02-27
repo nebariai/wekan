0b6599a22879d3b32c1b791987b601c59b1613c6
Omaid F <omaid@nebari.ai>
2026-02-27 14:30:43 -0800

    [BENCHMARK] Reintroduce CVE-2026-2207 in server/publications/activities.js

--- server/publications/activities.js
+++ server/publications/activities.js
@@ -21,38 +21,37 @@ Meteor.publish('activities', function(kind, id, limit, showActivities) {
     return this.ready();
   }
 
-  if (!this.userId) {
+  // Check user permissions - only BoardAdmin can view activities
+  if (this.userId) {
+    const user = ReactiveCache.getUser(this.userId);
+    const board = ReactiveCache.getBoard(id);
+    
+    if (user && board) {
+      // Find user membership in board
+      const membership = board.members.find(m => m.userId === this.userId);
+      
+      // Only BoardAdmin can view activities
+      if (!membership || !membership.isAdmin) {
+        return this.ready();
+      }
+    } else {
+      // If board or user not found, deny
+      return this.ready();
+    }
+  } else {
+    // If not logged in, deny
     return this.ready();
   }
 
+  // Get linkedBoard
   let linkedElmtId = [id];
-  let board;
-
-  if (kind === 'board') {
-    board = ReactiveCache.getBoard(id);
-    if (!board || !board.isVisibleBy(this.userId)) {
-      return this.ready();
-    }
-
-    // Get linked boards, but only those visible to the user
+  if (kind == 'board') {
     ReactiveCache.getCards({
       "type": "cardType-linkedBoard",
-      "boardId": id
-    }).forEach(card => {
-      const linkedBoard = ReactiveCache.getBoard(card.linkedId);
-      if (linkedBoard && linkedBoard.isVisibleBy(this.userId)) {
+      "boardId": id}
+      ).forEach(card => {
         linkedElmtId.push(card.linkedId);
-      }
     });
-  } else if (kind === 'card') {
-    const card = ReactiveCache.getCard(id);
-    if (!card) {
-      return this.ready();
-    }
-    board = ReactiveCache.getBoard(card.boardId);
-    if (!board || !board.isVisibleBy(this.userId)) {
-      return this.ready();
-    }
   }
 
   const selector = showActivities
